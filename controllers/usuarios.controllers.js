07502c420eaa34e6f4f15254da65cf82273154cd
agusmena <agusmena60@mail.com>
2024-07-02 15:44:59 -0300

    separo controller en services

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,36 +1,17 @@
-const usuarios = [
-  {
-    id: 1,
-    nombreDeUsuario: "andres2024",
-    emailDelUsuario: "andresperlo@gmail.com",
-    contrasenia: "123456789",
-  },
-];
-
+const serviceUsuario = require("../services/usuarios.services");
 const registrarUsuario = (req, res) => {
   try {
-    const body = req.body;
-    const emailExiste = usuarios.find(
-      (usuario) => usuario.emailDelUsuario === body.emailDelUsuario
-    );
-    const usuarioExiste = usuarios.find(
-      (usuario) => usuario.nombreDeUsuario === body.nombreDeUsuario
-    );
-    if (emailExiste) {
-      return res.status(400).json({ msg: "email no disponible" });
-    } else if (usuarioExiste) {
-      return res.status(400).json({ msg: "usuario no disponible" });
+    const res = serviceUsuario.nuevoUsuario(req.body);
+    if (res === 201) {
+      ///
     }
-    const id = crypto.randomUUID();
-    usuarios.push({ id, baja:false, ...body, });
-
-    res.status(200).json({ msg: "usuario resgistrado con exito" });
   } catch (error) {
     console.log(error);
   }
 };
 const obtenerTodosUsuarios = (req, res) => {
   try {
+    const usuarios = serviceUsuario.obtenerTodosUsuarios;
     res.status(200).json(usuarios);
   } catch (error) {
     console.log(error);
@@ -39,13 +20,7 @@ const obtenerTodosUsuarios = (req, res) => {
 
 const obtenerUnUsuario = (req, res) => {
   try {
-    const id = Number(req.params.idUsuario)
-    const usuario = usuarios.find((user) => user.id === id);
-    if(!usuario){
-        return res.status(400).json({msg:'Usuario no encontrado'})
-    }
-
-
+    const usuario = serviceUsuario.obtenerUnUsuario(req.params.idUsuario);
 
     res.status(200).json({ msg: "usuario encontrado", usuario });
   } catch (error) {
@@ -53,34 +28,30 @@ const obtenerUnUsuario = (req, res) => {
   }
 };
 
-const bajaFisicaUsuario =(req, res)=>{
-    try {
-        const id= req.params.idUsuario
-        const posicionDelUsuario = usuarios.findIndex((usuario)=> usuario.id === id)
-        usuarios.splice(posicionDelUsuario,1)
-        res.status(200).json(usuarios)
-    } catch (error) {
-        console.log(error)
+const bajaFisicaUsuario = (req, res) => {
+  try {
+    const res = serviceUsuario.bajaUsuarioFisica(req.params.idUsuario);
+    if (res === 200) {
+      res.status(200).json({ msg: "Usuario borrado con exito" });
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const bajaLogicaUsuario =(req, res)=>{
-    try {
-        const id= req.params.idUsuario
-        const posicionDelUsuario = usuarios.findIndex((usuario) => usuario.id === id)
-        usuarios[posicionDelUsuario].baja = !usuarios[posicionDelUsuario].baja
-       const mensaje = usuarios[posicionDelUsuario].baja ? 'Usuario bloqueado ': 'usario activo'
-        
-        res.status(200).json({msg:mensaje})
-    } catch (error) {
-        console.log(error)
-    }
-}
+const bajaLogicaUsuario = (req, res) => {
+  try {
+    const res = serviceUsuario.bajaUsuarioLogica(req.params.idUsuario);
+    res.status(200).json({ msg: res });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   registrarUsuario,
   obtenerTodosUsuarios,
   obtenerUnUsuario,
   bajaFisicaUsuario,
-  bajaLogicaUsuario
+  bajaLogicaUsuario,
 };
---
+++ services/usuarios.services.js
@@ -0,0 +1,73 @@
+const usuarios = [
+    {
+      id: 1,
+      nombreDeUsuario: "andres2024",
+      emailDelUsuario: "andresperlo@gmail.com",
+      contrasenia: "123456789",
+    },
+  ];
+
+const nuevoUsuario =(body)=>{
+    try {
+        const emailExiste = usuarios.find(
+          (usuario) => usuario.emailDelUsuario === body.emailDelUsuario
+        );
+        const usuarioExiste = usuarios.find(
+          (usuario) => usuario.nombreDeUsuario === body.nombreDeUsuario
+        );
+        if (emailExiste) {
+          return res.status(400).json({ msg: "email no disponible" });
+        } else if (usuarioExiste) {
+          return res.status(400).json({ msg: "usuario no disponible" });
+        }
+        const id = crypto.randomUUID();
+        usuarios.push({ id, baja:false, ...body, });
+    
+return 201
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const obtenerTodosUsuarios=()=>{
+    try {
+        return usuarios
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const obtenerUnUsuario = (usuario)=>{
+    try {
+        const usuario = usuarios.find((user)=> user.id === idUsuario)
+    
+    return usuario
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//baja fisica
+
+const bajaUsuarioFisica =(IdUsuario)=>{
+   const posicionDelUsuario = usuarios.findIndex((usuario)=> usuario.id ===idUsuario)
+usuarios.splice(posicionDelUsuario, 1)
+return 200
+}
+
+const bajaUsuarioLogica =(idUsuario)=>{
+    const posicionDelUsuario = usuarios.findIndex((usuario) => usuario.id=== idUsuario)
+ usuarios[posicionDelUsuario].baja = !usuarios[posicionDelUsuario].baja
+const mensaje = usuarios[posicionDelUsuario].baja? 'Usuario Bloqueado' : 'Usuario Activo'
+return mensaje
+}
+
+module.exports={
+    nuevoUsuario,
+    obtenerTodosUsuarios,
+    obtenerUnUsuario, 
+    bajaUsuarioFisica,
+    bajaUsuarioLogica
+}
